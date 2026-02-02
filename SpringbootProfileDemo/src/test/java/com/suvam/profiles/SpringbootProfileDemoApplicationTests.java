e08d432da8902030c9f35f7c81ca22098c634ef8
Suvam Debnath <debnathsuvam322@gmail.com>
2026-02-03 00:10:17 +0530

    added springboot profiles

---
+++ SpringbootProfileDemo/src/test/java/com/suvam/profiles/SpringbootProfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suvam.profiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProfileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
