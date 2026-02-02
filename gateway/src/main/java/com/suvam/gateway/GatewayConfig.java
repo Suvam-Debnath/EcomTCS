8118a6adb967b4223c785017ab8b19b70b8b6bf7
Suvam Debnath <debnathsuvam322@gmail.com>
2026-02-02 23:24:05 +0530

    added circuitbreaker and fallback

--- gateway/src/main/java/com/suvam/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/suvam/gateway/GatewayConfig.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 
 @Configuration
 public class GatewayConfig {
@@ -13,15 +12,23 @@ public class GatewayConfig {
         return builder.routes()
                 .route("product", r -> r
                         .path("/api/products/**")
-
+                        .filters(f->f.circuitBreaker(config -> config
+                                        .setName("ecomBreaker")
+                                        .setFallbackUri("forward:/fallback/products")))
                         .uri("lb://PRODUCT"))
 
                 .route("user", r -> r
                         .path("/api/users/**")
+                        .filters(f->f.circuitBreaker(config -> config
+                                .setName("ecomBreaker")
+                                .setFallbackUri("forward:/fallback/users")))
                         .uri("lb://USER"))
 
                 .route("order", r -> r
                         .path("/api/orders/**", "/api/cart/**")
+                        .filters(f->f.circuitBreaker(config -> config
+                                .setName("ecomBreaker")
+                                .setFallbackUri("forward:/fallback/orders")))
                         .uri("lb://ORDER"))
 
                 .route("eureka", r -> r
